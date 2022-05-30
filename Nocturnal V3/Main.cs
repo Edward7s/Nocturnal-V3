331b1c390e40bf53b4afeb16bc44d45fd7a26b5b
Edward7 <ediro174@gmail.com>
2022-05-30 23:22:31 +0300

    Small Change new buttons on user and world

--- Nocturnal V3/Main.cs
+++ Nocturnal V3/Main.cs
@@ -174,14 +174,27 @@ namespace Nocturnal
 
         public static void Update()
         {
+
+
+
+
            if (Settings.ConfigVars.discordrichpresence)
             Settings.Download_Files.callback.Invoke(Settings.Download_Files.callback, null);
 
             //   discord.RunCallbacks();
 
         
-            if (Input.GetKeyDown(KeyCode.K))
+         /*   if (Input.GetKeyDown(KeyCode.K))
             {
+               var btn =  GameObject.Find("/UserInterface").transform.Find("Canvas_QuickMenu(Clone)/Container/Window/QMParent/Menu_SelectedUser_Local/ScrollRect/Viewport/VerticalLayoutGroup/UserProfile_Compact/PanelBG/Cell_QM_User");
+                var list = btn.GetComponent<UnityEngine.UI.Button>().onClick.m_PersistentCalls;
+              //  NocturnalC.log(btn.GetComponent<UnityEngine.UI.Button>().onClick.GetDelegate();
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                   // NocturnalC.log(list[i]);
+                }
+
              //   var bot = GameObject.Find("/Decor/Vroomba").gameObject;
              //   var hg = VRC.Player.prop_Player_0.gameObject.transform.Find("AnimationController/HeadAndHandIK/RightEffector").GetComponent<VRCHandGrasper>();
           //   hg.field_Internal_VRC_Pickup_0 = bot.GetComponent<VRC_Pickup>();
@@ -189,7 +202,7 @@ namespace Nocturnal
            //   hg.Method_Public_VRC_Pickup_0();
          //    hg.Method_Public_VRC_Pickup_1();
 
-            }
+            }*/
 
 
             try
--- Nocturnal V3/Ui/buttons b.cs
+++ Nocturnal V3/Ui/buttons b.cs
@@ -17,35 +17,37 @@ namespace Nocturnal.Ui
         {
             var addnewgmj = new GameObject();
             var grid = addnewgmj.AddComponent<GridLayoutGroup>();
-            grid.cellSize = new Vector2(200, 100);
+            grid.cellSize = new Vector2(135, 100);
             grid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
-            grid.constraintCount = 6;
+            grid.constraintCount = 9;
             addnewgmj.name = "N_VBTNHOLDER";
             addnewgmj.transform.parent = objects.userinfpannel.transform.Find("User Panel").transform;
-            addnewgmj.transform.localPosition = new Vector3(49.7516f, -284.6267f, 0);
+            addnewgmj.transform.localPosition = new Vector3(49.7516f, -281.5556f, 0);
             addnewgmj.transform.localScale = Vector3.one;
+            addnewgmj.transform.localEulerAngles = Vector3.zero;
+            var userp = GameObject.Find("/UserInterface").transform.Find("MenuContent/Screens/UserInfo").gameObject.GetComponent<VRC.UI.PageUserInfo>();
+
             BButton.NormalButton("Target User", addnewgmj, () => Nocturnal.Settings.wrappers.Target.Targetuser(GameObject.Find("/UserInterface").transform.Find("MenuContent/Screens/UserInfo").gameObject.GetComponent<VRC.UI.PageUserInfo>().field_Private_APIUser_0.id));
             BButton.NormalButton("Teleport", addnewgmj, () =>
             {
 
-                var User = GameObject.Find("/UserInterface").transform.Find("MenuContent/Screens/UserInfo").gameObject.GetComponent<VRC.UI.PageUserInfo>().field_Private_APIUser_0.id;
                 try
                 {
-                    VRC.Player.prop_Player_0.transform.position = User.getuserbyid().transform.position;
+                    VRC.Player.prop_Player_0.transform.position = userp.field_Private_APIUser_0.id.getuserbyid().transform.position;
               
                 }
                 catch { }
 
             });
+
             BButton.NormalButton("Force Clone", addnewgmj, () =>
             {
                 var aviid = "";
-                var User = GameObject.Find("/UserInterface").transform.Find("MenuContent/Screens/UserInfo").gameObject.GetComponent<VRC.UI.PageUserInfo>().field_Private_APIUser_0.id;
                 try
                 {
-                    var user = User.getuserbyid();
+                    var user = userp.field_Private_APIUser_0.id.getuserbyid();
 
-                    if (user.field_Private_APIUser_0.id == User)
+                    if (user.field_Private_APIUser_0.id == userp.field_Private_APIUser_0.id)
                             aviid = user.prop_ApiAvatar_0.id;
                     
                     exploits.misc.changetoavi(aviid);
@@ -55,6 +57,26 @@ namespace Nocturnal.Ui
 
             });
             BButton.NormalButton("Copy uid", addnewgmj, () => System.Windows.Forms.Clipboard.SetText(GameObject.Find("/UserInterface").transform.Find("MenuContent/Screens/UserInfo").gameObject.GetComponent<VRC.UI.PageUserInfo>().field_Private_APIUser_0.id));
+
+            BButton.NormalButton("Copy avi id", addnewgmj, () =>
+            {
+                try
+                {
+                    userp.field_Private_APIUser_0.Fetch();
+                    NocturnalC.log(userp.field_Private_APIUser_0.avatarId);
+                    System.Windows.Forms.Clipboard.SetText(userp.field_Private_APIUser_0.avatarId);
+
+                }
+                catch { }
+               
+            });
+
+            BButton.NormalButton("Copy avi img", addnewgmj, () =>
+            {
+                NocturnalC.log(userp.field_Private_APIUser_0.currentAvatarImageUrl);
+                System.Windows.Forms.Clipboard.SetText(userp.field_Private_APIUser_0.currentAvatarImageUrl);
+            });
+
 
             buttonaddtag = BButton.NormalButton("Set Tag", addnewgmj, () =>
             {
@@ -76,7 +98,7 @@ namespace Nocturnal.Ui
 
                         code = "9",
 
-                        msg2 = buttonaddtag.transform.parent.parent.parent.GetComponent<VRC.UI.PageUserInfo>().field_Private_APIUser_0.id,
+                        msg2 = userp.field_Private_APIUser_0.id,
 
                         msg = tagtosend.Trim(),
 
@@ -95,6 +117,46 @@ namespace Nocturnal.Ui
             biguiscrollbar.setscrollbars();
             Bundles.loadrain();
 
+
+
+
+
+
+
+            var worldinfogmj = GameObject.Find("/UserInterface").transform.Find("MenuContent/Screens/WorldInfo").transform;
+            var worldinfo = worldinfogmj.GetComponent<VRC.UI.PageWorldInfo>();
+            var newworlgmj = new GameObject();
+            var newgrid = newworlgmj.AddComponent<GridLayoutGroup>();
+           // grid.cellSize = new Vector2(200, 100);
+            newgrid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            newgrid.constraintCount = 3;
+            newworlgmj.name = "N_VBTNHOLDER";
+            newworlgmj.transform.parent = worldinfogmj;
+            newworlgmj.transform.localPosition = new Vector3(-475, 320, 0);
+            newworlgmj.transform.localScale = Vector3.one;
+            newworlgmj.transform.localEulerAngles = Vector3.zero;
+            BButton.NormalButton("Copy Id", newworlgmj, () =>
+            {
+                NocturnalC.log(worldinfo.field_Public_ApiWorldInstance_0.id);
+                System.Windows.Forms.Clipboard.SetText(worldinfo.field_Public_ApiWorldInstance_0.id);
+            });
+            BButton.NormalButton("Creator Id", newworlgmj, () =>
+            {
+                NocturnalC.log(worldinfo.field_Public_APIUser_0.id);
+                System.Windows.Forms.Clipboard.SetText(worldinfo.field_Public_APIUser_0.id);
+            });
+            BButton.NormalButton("Asset Url", newworlgmj, () =>
+            {
+                NocturnalC.log(worldinfo.field_Private_ApiWorld_0.assetUrl);
+                System.Windows.Forms.Clipboard.SetText(worldinfo.field_Private_ApiWorld_0.assetUrl);
+                Application.OpenURL(worldinfo.field_Private_ApiWorld_0.assetUrl);
+            });
+            BButton.NormalButton("Copy Img", newworlgmj, () =>
+            {
+                NocturnalC.log(worldinfo.field_Private_ApiWorld_0.imageUrl);
+                System.Windows.Forms.Clipboard.SetText(worldinfo.field_Private_ApiWorld_0.imageUrl);
+            });
         }
+
     }
 }
